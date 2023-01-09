573712a8a17219cbc2bfcfb4d0b3764f4c80c28a
ankitladva11 <ankit786ladva@gmail.com>
2023-01-09 23:51:35 +0530

    added

---
+++ C++_DSA/preorder.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void traverse(TreeNode* root, vector<int>& v) {
+        if (root==NULL) return;     // if node is null there is nothing more to check
+        v.push_back(root->val);     // We first add the node we're visiting to the output vector
+        traverse(root->left, v);    // Now let's move through the left siblings
+        traverse(root->right, v);   // After the left siblings are done with, we check for the right siblings as we start going back
+    }
+
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> v;
+        int kkk=1;
+        traverse(root, v);
+        return v;
+    }
+};
